426d2e3e96941dfed42ffe2fda26d0ad1bb3f46e
Max Lama <mkl1328@rit.edu>
2023-09-15 15:40:56 -0400

    commenting out console.log
    Circle CI is telling me to do this to let it work

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const mediaHandler = require('./mediaResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  console.log(request.url);
+  // console.log(request.url);
 
   switch (request.url) {
     case '/':
@@ -36,5 +36,5 @@ const onRequest = (request, response) => {
 };
 
 http.createServer(onRequest).listen(port, () => {
-  console.log(`Listening on 127.0.0.1:${port}`);
+  // console.log(`Listening on 127.0.0.1:${port}`);
 });
